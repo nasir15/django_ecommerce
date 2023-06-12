67d8f91d40f7f4b159e8627bb85d90c3ce0f2597
mnasir.khan <gitlablfd>
2023-06-12 15:11:20 +0500

    remove base url from images

--- api/views.py
+++ api/views.py
@@ -12,14 +12,10 @@ class ProductAPI(APIView):
     def get(self, request):
         try:
             products = Product.objects.all()
-            print("products ",products)
+            # print("products ",products)
             serializer = ProductSerializer(products, many=True)
-            print("Serializer data",serializer.data)
-            base_url="http://192.168.53.105:8080"
-            for i in serializer.data:
-                if i.get('image') is not None:
-                    i['image']=base_url+i.get('image')
-                       
+            # print("Serializer data",serializer.data)
+            
             return Response(serializer.data, status=200)
         except Exception as error:
             return Response({'error': repr(error), 'error_code': 'H007', 'matched': 'N', 'data': {}}, status=400)
