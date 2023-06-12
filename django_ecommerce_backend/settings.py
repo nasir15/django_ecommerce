f249334096890b3e06af74b1a7b5640cb1bf851a
mnasir.khan <gitlablfd>
2023-06-12 11:34:04 +0500

    sqlite db added

--- django_ecommerce_backend/settings.py
+++ django_ecommerce_backend/settings.py
@@ -92,18 +92,18 @@ DATABASES = {
     }
 }
 
-DATABASES = {
+# DATABASES = {
 
-'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': config('DB_NAME'),
-    'USER': config('DB_USER'),
-    'PASSWORD': config('DB_PASSWORD'),
-    'HOST': config('DB_HOST'),
-    'PORT': config('DB_PORT'),
-  },
+# 'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': config('DB_NAME'),
+#     'USER': config('DB_USER'),
+#     'PASSWORD': config('DB_PASSWORD'),
+#     'HOST': config('DB_HOST'),
+#     'PORT': config('DB_PORT'),
+#   },
 
-}
+# }
 
 
 # Password validation
